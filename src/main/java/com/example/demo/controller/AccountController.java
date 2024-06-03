8b8250d7fe88b1beb648a91add498c7de2e0f062
394-student015 <394-student015@la-study.com>
2024-06-03 14:57:15 +0900

    更新ゆう

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -2,13 +2,31 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.Account;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AccountController {
 
 	@Autowired
 	Account account;
+	@Autowired
+	HttpSession session;
+
+	@GetMapping("/")
+	public String index() {
+		session.invalidate();
+		return "login";
+	}
+
+	@PostMapping("/login")
+	public String login() {
+
+		return "menu";
+	}
 
 }
