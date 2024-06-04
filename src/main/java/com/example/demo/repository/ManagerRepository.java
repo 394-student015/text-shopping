514e4f3f7aed8d57bd72bf46b6a2579cf67030e9
394-student015 <394-student015@la-study.com>
2024-06-04 09:49:18 +0900

    account managerを全体的に更新しっました。

--- src/main/java/com/example/demo/repository/ManagerRepository.java
+++ src/main/java/com/example/demo/repository/ManagerRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.repository;
 
-public class ManagerRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Manager;
+
+public interface ManagerRepository extends JpaRepository<Manager, Integer> {
+
+	List<Manager> findByPassword(String password);
+
 	//内部設計書CL012参照
 }
