0ba61d79625703d76dbc2da17e1563d7ecb5637a
394-student016 <394-student016@la-study.com>
2024-06-04 09:38:42 +0900

    Professor暫定完成

--- src/main/java/com/example/demo/controller/ProfessorController.java
+++ src/main/java/com/example/demo/controller/ProfessorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -37,4 +38,34 @@ public class ProfessorController {
 		return "professorAdd";
 	}
 
+	//教授更新画面表示
+	@GetMapping("/professor/{id}/edit")
+	public String updateProfessor(
+			@PathVariable("id") Integer id,
+			Model model) {
+		Professor professor = professorRepository.findById(id).get();
+		model.addAttribute("professor", professor);
+		return "professorUpdate";
+	}
+
+	//教授更新
+	@PostMapping("/professor/{id}/edit")
+	public String updateProfessor(
+			@PathVariable("id") Integer id,
+			@RequestParam(value = "name", defaultValue = "") String name,
+			@RequestParam(value = "major", defaultValue = "") String major,
+			Model model) {
+		Professor professor = new Professor(id, name, major);
+		professorRepository.save(professor);
+		return "redirect:/professor";
+	}
+
+	//教授削除
+	@PostMapping("/professor/{id}/delete")
+	public String deleteProfessorf(
+			@PathVariable("id") Integer id,
+			Model model) {
+		professorRepository.deleteById(id);
+		return "redirect:/professor";
+	}
 }
