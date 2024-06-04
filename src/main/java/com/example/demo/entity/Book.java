6811292a62f644d0e33bb149ea70f97eb50df03b
394-student011 <394-student011@la-study.com>
2024-06-04 17:15:07 +0900

    View作った。 entityに「Book.java」を作成した。

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,78 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+//View作った。HTMLでもv_textbookを使う。
+
+@Entity
+@Table(name = "v_textbook")
+public class Book {
+
+	private int id;
+	private String title;
+	private String author;
+	private int price;
+	private int stock;
+	private String professor;
+	private String lecture;
+
+	public Book() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getProfessor() {
+		return professor;
+	}
+
+	public void setProfessor(String professor) {
+		this.professor = professor;
+	}
+
+	public String getLecture() {
+		return lecture;
+	}
+
+	public void setLecture(String lecture) {
+		this.lecture = lecture;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+}
--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -1,2 +1,3 @@
-select class.name,professor.name,textbook.id,textbook.title,textbook.author,textbook.price,textbook.stock,textbook.professor_id,textbook.class_id 
+create view v_textbook as
+select class.name as lecture,professor.name as professor,textbook.id,textbook.title,textbook.author,textbook.price,textbook.stock 
 from textbook join professor on professor.id=textbook.professor_id join class on class.id=textbook.class_id;
