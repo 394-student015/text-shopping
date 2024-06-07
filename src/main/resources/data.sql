2646634ad9a75303c11ec3add4d20d7e49b0a823
394-student002 <394-student002@la-study.com>
2024-06-07 10:24:27 +0900

    教科書検索機能を実装しました。

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,6 @@
 INSERT INTO professor(name,major) VALUES('アンパンマン','量子宇宙論');
 INSERT INTO lesson(name) VALUES('水中におけるダンスの効用');
 
-
-
 -- 商品テーブルデータ
 INSERT INTO textbook(title,author,price,stock,professor_id,lesson_id) VALUES('Javaの基本', '佐藤一郎',2500,3,1,1);
 INSERT INTO textbook(title,author,price,stock,professor_id,lesson_id) VALUES('ごんぎつね', '佐藤二郎',2000,4,1,1);
